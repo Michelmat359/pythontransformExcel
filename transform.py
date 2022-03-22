4f148ad1138b175cdc3d5806557d4bd06f1a7b6c
Miguel Angel Mateo Casali <mmateo@cigip.upv.es>
2022-03-22 15:36:13 +0100

    update

--- transform.py
+++ transform.py
@@ -1,98 +1,109 @@
 import csv
-# import numpy as np
-from csv import DictWriter
 
-class Transformdata:
 
-    def __init__(self, id:int, ts:float, tn:float, ti:float, time):
-        self.id = id
-        self.ts = ts
-        self.tn = tn
-        self.ti = ti
-        self.time = time
-    
-    def read ():
+
+class Transformdata():
+
+    # constructor
+    def __init__(self):
+        # Initialization
+        self.num_tool = 8
+        self.start_data = 21
+        self.finish_data = 78
+
+    def read(self):
         with open("CalidadViejo.csv") as file_name:
-            
+
             file_read = csv.reader(file_name)
-            row_count = 0 
+            row_count = 0
+            final_list = []
             for row in file_read:
                 row_count = row_count + 1
-                if(row_count == 2):          
+                if (row_count == 2):
                     first_data = row[0].split(";")
                     array_id = []
-                    array_id.append(first_data[1]) # Reference
-                    array_id.append(first_data[5]) # Id tool 1
-                    array_id.append(first_data[11]) # Id tool 2
-                    array_id.append(first_data[17]) # Id tool 3
-                    array_id.append(first_data[23]) # Id tool 4
-                    array_id.append(first_data[29]) # Id tool 5
-                    array_id.append(first_data[35]) # Id tool 6
-                    array_id.append(first_data[41]) # Id tool 7
-                    array_id.append(first_data[47]) # Id tool 8    
-                if(row_count == 3):
+                    array_id.append(float(first_data[1]))  # Reference
+
+                    for i in range(5, ((self.num_tool) * 6) + 5):
+                        if (first_data[i] != ""):
+                            array_id.append(float(first_data[i]))
+                    print(array_id)
+
+                if (row_count == 3):
                     first_data = row[0].split(";")
                     array_valuets = []
-                    array_valuets.append(first_data[0]) # Ts
-                    array_valuets.append(first_data[5]) # Value Ts Id tool 1
-                    array_valuets.append(first_data[11]) # Value Ts Id tool 2
-                    array_valuets.append(first_data[17]) # Value Ts Id tool 3
-                    array_valuets.append(first_data[23]) # Value Ts Id tool 4
-                    array_valuets.append(first_data[29]) # Value Ts Id tool 5
-                    array_valuets.append(first_data[35]) # Value Ts Id tool 6
-                    array_valuets.append(first_data[41]) # Value Ts Id tool 7
-                    array_valuets.append(first_data[47]) # Value Ts Id tool 8    
-                if(row_count == 4):
+                    array_valuets.append(first_data[0])  # Ts
+
+                    for i in range(5, ((self.num_tool) * 6) + 5):
+                        if (first_data[i] != ""):
+                            array_valuets.append(float(first_data[i]))
+                    print(array_valuets)
+                if (row_count == 4):
                     first_data = row[0].split(";")
                     array_valuetn = []
-                    array_valuetn.append(first_data[0]) # TN
-                    array_valuetn.append(first_data[5]) # Value TN Id tool 1
-                    array_valuetn.append(first_data[11]) # Value TN Id tool 2
-                    array_valuetn.append(first_data[17]) # Value TN Id tool 3
-                    array_valuetn.append(first_data[23]) # Value TN Id tool 4
-                    array_valuetn.append(first_data[29]) # Value TN Id tool 5
-                    array_valuetn.append(first_data[35]) # Value TN Id tool 6
-                    array_valuetn.append(first_data[41]) # Value TN Id tool 7
-                    array_valuetn.append(first_data[47]) # Value TN Id tool 8    
-                if(row_count == 5):
+                    array_valuetn.append(first_data[0])  # TN
+
+                    for i in range(5, ((self.num_tool) * 6) + 5):
+                        if (first_data[i] != ""):
+                            array_valuetn.append(float(first_data[i]))
+                    print(array_valuetn)
+                if (row_count == 5):
                     first_data = row[0].split(";")
                     array_valueti = []
-                    array_valueti.append(first_data[0]) # TN
-                    array_valueti.append(first_data[5]) # Value TN Id tool 1
-                    array_valueti.append(first_data[11]) # Value TN Id tool 2
-                    array_valueti.append(first_data[17]) # Value TN Id tool 3
-                    array_valueti.append(first_data[23]) # Value TN Id tool 4
-                    array_valueti.append(first_data[29]) # Value TN Id tool 5
-                    array_valueti.append(first_data[35]) # Value TN Id tool 6
-                    array_valueti.append(first_data[41]) # Value TN Id tool 7
-                    array_valueti.append(first_data[47]) # Value TN Id tool 8    
-            final_array = []
+                    array_valueti.append(first_data[0])  # TN
+                    for i in range(5, ((self.num_tool) * 6) + 5):
+                        if (first_data[i] != ""):
+                            array_valueti.append(float(first_data[i]))
+                    print(array_valueti)
+                if (row_count >= self.start_data and row_count <= self.finish_data):
+                    line = []
+                    data_rowdata = row[0].split(";")
+                    line.append(data_rowdata[0])
+                    line.append(data_rowdata[1])
+                    line.append(data_rowdata[2])
+                    aux = 0
+                    for i in range(5, ((self.num_tool - 1) * 6) + 5):
+                        if (data_rowdata[i] != ""):
+                            aux = aux +1
+                            line.append(float(data_rowdata[i]))
+                            if(float(data_rowdata[i]) - float(array_valuetn[aux])== 0):
+                                line.append(3)
+                            elif (float(data_rowdata[i]) - float(array_valuetn[aux])< 0 and float(data_rowdata[i]) >= float(array_valueti[aux])):
+                                line.append(2)
+                            elif (float(data_rowdata[i]) - float(array_valuetn[aux]) > 0 and float(data_rowdata[i]) <= float(array_valuets[aux])):
+                                line.append(1)
+                            else:
+                                line.append(0)
+                            line.append(round(float(data_rowdata[i]) - float(array_valuetn[aux]),3))
+                    final_list.append(line)
+
+
+            final_array_head = []
+            arraY_vacio = []
             x = 0
             while x < len(array_valueti):
-                if(x == 0):
-                    final_array.append(array_id[x])
+                if (x == 0):
+                    final_array_head.append(array_id[x])
                 else:
-                    final_array.append(array_id[x])
-                    final_array.append(array_valuets[x])
-                    final_array.append(array_valuetn[x])
-                    final_array.append(array_valueti[x])
-                x+=1
-            print(final_array)
-            return final_array
+                    final_array_head.append(array_id[x])
+                    final_array_head.append(array_valuets[x])
+                    final_array_head.append(array_valuetn[x])
+                    final_array_head.append(array_valueti[x])
+                x += 1
+
+            final_list.insert(0, final_array_head)
+            final_list.insert(1, arraY_vacio)
+            self.write('sol3.csv', final_list)
 
 
+    def write(self, file_name, list_of_elem):
 
-    def append_list_as_row(file_name, list_of_elem):
-        with open(file_name, 'a+', newline=',') as write_obj:
+        with open(file_name, 'a+', newline='') as write_obj:
             csv_writer = csv.writer(write_obj)
-            csv_writer.writerow(list_of_elem)
+
+            csv_writer.writerows(([list_of_elem[index]] for index in range(0, len(list_of_elem))))
 
 
-    first_line = read()
-    append_list_as_row('sol.csv', first_line)
-
-
-
-
-
+Object = Transformdata()
+Object.read()
 
